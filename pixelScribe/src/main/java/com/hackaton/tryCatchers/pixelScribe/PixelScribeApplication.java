ce2198719beaea719001c3de82d94655bf4f8674
unknown <sergio.idarraga-t@labinfo.is.escuelaing.edu.co>
2025-11-04 14:31:14 -0500

    Inicialización proyecto maven con Spring Initlzr

---
+++ pixelScribe/src/main/java/com/hackaton/tryCatchers/pixelScribe/PixelScribeApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.tryCatchers.pixelScribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelScribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelScribeApplication.class, args);
+	}
+
+}
---
+++ pixelScribe/src/test/java/com/hackaton/tryCatchers/pixelScribe/PixelScribeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton.tryCatchers.pixelScribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelScribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
